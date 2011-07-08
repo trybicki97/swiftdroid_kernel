626f361ae181371cdae8c69ea8f018e1f0637d4e
Mur4ik <mur4ik@gmail.com>
2011-07-08 05:57:28 +0300

    Add ability to overclocking

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -212,7 +212,41 @@ static struct clkctl_acpu_speed pll0_960_pll1_245_pll2_1200[] = {
 	{ 1, 320000, ACPU_PLL_0, 4, 2, 160000, 1, 5, 122880 },
 	{ 0, 400000, ACPU_PLL_2, 2, 2, 133333, 2, 5, 122880 },
 	{ 1, 480000, ACPU_PLL_0, 4, 1, 160000, 2, 6, 122880 },
-	{ 1, 600000, ACPU_PLL_2, 2, 1, 200000, 2, 7, 122880 },
+/*	{ 1, 600000, ACPU_PLL_2, 2, 1, 200000, 2, 7, 122880 },
+	{ 1, 710400, ACPU_PLL_0, 4, 0, 236800, 2, 7, 200000 },
+	{ 1, 729600, ACPU_PLL_0, 4, 0, 182400, 3, 7, 122880 },
+	{ 1, 744000, ACPU_PLL_0, 4, 0, 186000, 3, 7, 122880 },
+	{ 1, 764800, ACPU_PLL_0, 4, 0, 191200, 3, 7, 122880 },
+	{ 1, 768000, ACPU_PLL_0, 4, 0, 192000, 3, 7, 122880 },
+	{ 1, 787200, ACPU_PLL_0, 4, 0, 196800, 3, 7, 122880 },
+	{ 1, 806400, ACPU_PLL_0, 4, 0, 201600, 3, 7, 122880 },
+	{ 1, 825600, ACPU_PLL_0, 4, 0, 206400, 3, 7, 122880 },
+	{ 1, 844800, ACPU_PLL_0, 4, 0, 211200, 3, 7, 122880 },
+	{ 1, 852000, ACPU_PLL_0, 4, 0, 213000, 3, 7, 122880 },
+	{ 1, 864000, ACPU_PLL_0, 4, 0, 216000, 3, 7, 122880 },
+	{ 1, 880000, ACPU_PLL_0, 4, 0, 220000, 3, 7, 122880 },
+	{ 1, 892000, ACPU_PLL_0, 4, 0, 223000, 3, 7, 122880 },
+	{ 1, 900000, ACPU_PLL_0, 4, 0, 225000, 3, 7, 122880 },*/
+//	{ 1, 952000, ACPU_PLL_0, 4, 0, 236000, 3, 7, 122880 },
+//	{ 1, 1000000, ACPU_PLL_0, 4, 0, 250000, 3, 7, 122880 },
+	{ 1, 600000, ACPU_PLL_2, 2, 1, 200000, 2, 7, 200000 },
+	{ 1, 652800, ACPU_PLL_0, 4, 0, 217600, 2, 7, 200000 },
+	{ 1, 672000, ACPU_PLL_0, 4, 0, 224000, 2, 7, 200000 },
+	{ 1, 691200, ACPU_PLL_0, 4, 0, 230400, 2, 7, 200000 },
+	{ 1, 710400, ACPU_PLL_0, 4, 0, 236800, 2, 7, 200000 },
+	{ 1, 729600, ACPU_PLL_0, 4, 0, 243200, 2, 7, 200000 },
+	{ 1, 748800, ACPU_PLL_0, 4, 0, 249600, 2, 7, 200000 },
+	{ 1, 764800, ACPU_PLL_0, 4, 0, 191200, 3, 7, 122880 },
+	{ 1, 768000, ACPU_PLL_0, 4, 0, 256000, 2, 7, 200000 },
+	{ 1, 787200, ACPU_PLL_0, 4, 0, 262400, 2, 7, 200000 },
+	{ 1, 806400, ACPU_PLL_0, 4, 0, 268800, 2, 7, 200000 },
+	{ 1, 825600, ACPU_PLL_0, 4, 0, 275200, 2, 7, 200000 },
+	{ 1, 844800, ACPU_PLL_0, 4, 0, 281600, 2, 7, 200000 },
+	{ 1, 852000, ACPU_PLL_0, 4, 0, 213000, 3, 7, 122880 },
+	{ 1, 864000, ACPU_PLL_0, 4, 0, 216000, 3, 7, 122880 },
+	{ 1, 880000, ACPU_PLL_0, 4, 0, 220000, 3, 7, 122880 },
+	{ 1, 892000, ACPU_PLL_0, 4, 0, 223000, 3, 7, 122880 },
+	{ 1, 900000, ACPU_PLL_0, 4, 0, 225000, 3, 7, 122880 },
 	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0}, {0, 0, 0} }
 };
 
@@ -263,7 +297,7 @@ static struct pll_freq_tbl_map acpu_freq_tbl_list[] = {
 };
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[20];
+static struct cpufreq_frequency_table freq_table[25];
 
 static void __init cpufreq_table_init(void)
 {
@@ -390,14 +424,19 @@ static int acpuclk_set_vdd_level(int vdd)
 
 /* Set proper dividers for the given clock speed. */
 static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s) {
-	uint32_t reg_clkctl, reg_clksel, clk_div, src_sel;
-
+	uint32_t reg_clkctl, reg_clksel, clk_div, src_sel, a11_div;
 	reg_clksel = readl(A11S_CLK_SEL_ADDR);
 
 	/* AHB_CLK_DIV */
 	clk_div = (reg_clksel >> 1) & 0x03;
 	/* CLK_SEL_SRC1NO */
 	src_sel = reg_clksel & 1;
+	a11_div = hunt_s->a11clk_src_div;
+	if(hunt_s->a11clk_khz>600000) {
+		a11_div=0;
+		writel(hunt_s->a11clk_khz/19200, PLLn_L_VAL(0));
+		udelay(50);
+	}
 
 	/*
 	 * If the new clock divider is higher than the previous, then
@@ -412,7 +451,10 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s) {
 	/* Program clock source and divider */
 	reg_clkctl = readl(A11S_CLK_CNTL_ADDR);
 	reg_clkctl &= ~(0xFF << (8 * src_sel));
+	reg_clkctl |=a11_div;
 	reg_clkctl |= hunt_s->a11clk_src_sel << (4 + 8 * src_sel);
+
+	reg_clkctl |=a11_div;
 	reg_clkctl |= hunt_s->a11clk_src_div << (0 + 8 * src_sel);
 	writel(reg_clkctl, A11S_CLK_CNTL_ADDR);
 
