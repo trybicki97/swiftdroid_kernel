a09cfa63d65e4de71dedc65d6d0142c7527f3dd8
Mur4ik <mur4ik@gmail.com>
2011-07-12 12:58:05 +0300

    Remove headphone insert swtich.
    This switch is making Android think the device has a hardware keyboard and removing it appears to have no side effects.

--- arch/arm/mach-msm/rpc_server_handset.c
+++ arch/arm/mach-msm/rpc_server_handset.c
@@ -566,7 +566,7 @@ static int __devinit hs_probe(struct platform_device *pdev)
 
 	input_set_capability(ipdev, EV_KEY, KEY_HP);
 	input_set_capability(ipdev, EV_KEY, KEY_MEDIA);
-	input_set_capability(ipdev, EV_SW, SW_HEADPHONE_INSERT);
+	//input_set_capability(ipdev, EV_SW, SW_HEADPHONE_INSERT);
 	input_set_capability(ipdev, EV_KEY, KEY_POWER);
 	input_set_capability(ipdev, EV_KEY, KEY_END);
 
